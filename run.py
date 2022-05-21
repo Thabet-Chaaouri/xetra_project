5a95e3844e8e85c4885fb46b3430e3346c9f6c8e
unknown <thabet.chaawri@gmail.com>
2022-05-21 22:00:22 +0200

    code implementation

--- run.py
+++ run.py
@@ -1,16 +1,56 @@
+"""Running the Xetra ETL application"""
+import argparse
 import logging
 import logging.config
+
 import yaml
+
+from xetra.common.s3 import S3BucketConnector
+from xetra.transformers.xetra_transformer import XetraETL, XetraSourceConfig, XetraTargetConfig
 
 
 def main():
-    # Init
-    config_path = "C:/Users/thabe/xetra_project/xetra_project/configs/xetra_report1_config.yml"
-    config = yaml.safe_load(open(config_path))
+    """
+        entry point to run the xetra ETL job.
+    """
+    # Parsing YML file
+    parser = argparse.ArgumentParser(description='Run the Xetra ETL Job.')
+    parser.add_argument('config', help='A configuration file in YAML format.')
+    args = parser.parse_args()
 
+#    config = 'C:/Daten/xetra_project_old/xetra_1234/configs/xetra_report1_config.yaml'
+#    config = yaml.safe_load(open(config))
+
+    config = yaml.safe_load(open(args.config))
+    # configure logging
     log_config = config['logging']
     logging.config.dictConfig(log_config)
+    # reading s3 configuration
+    s3_config = config['s3']
+    # creating the S3BucketConnector classes for source and target
+    s3_bucket_src = S3BucketConnector(access_key=s3_config['access_key'],
+                                      secret_key=s3_config['secret_key'],
+                                      endpoint_url=s3_config['src_endpoint_url'],
+                                      bucket=s3_config['src_bucket'])
+    s3_bucket_trg = S3BucketConnector(access_key=s3_config['access_key'],
+                                      secret_key=s3_config['secret_key'],
+                                      endpoint_url=s3_config['trg_endpoint_url'],
+                                      bucket=s3_config['trg_bucket'])
+    # reading source configuration
+    source_config = XetraSourceConfig(**config['source'])
+    # reading target configuration
+    target_config = XetraTargetConfig(**config['target'])
+    # reading meta file configuration
+    meta_config = config['meta']
+    # creating XetraETL class
     logger = logging.getLogger(__name__)
-    logger.info("this is a test")
+    logger.info('Xetra ETL job started.')
+    xetra_etl = XetraETL(s3_bucket_src, s3_bucket_trg,
+                         meta_config['meta_key'], source_config, target_config)
+    # running etl job for xetra report 1
+    xetra_etl.etl_report1()
+    logger.info('Xetra ETL job finished.')
+
+
 if __name__ == '__main__':
     main()
--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -1,7 +1,13 @@
 """ Connector  and methods accessing s3"""
 import os
 import logging
+from io import StringIO, BytesIO
 import boto3
+import pandas as pd
+
+from xetra.common.constants import S3FileTypes
+from xetra.common.custom_exceptions import WrongFormatException
+
 
 class S3BucketConnector():
     """
@@ -23,16 +29,46 @@ class S3BucketConnector():
         self._s3 = self.session.resource(service_name='s3', endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
 
-    def read_csv_to_df(bucket, key, decoding = 'utf-8', sep = ','):
-        csv_obj = bucket.Object(key=key).get().get('Body').read().decode(decoding)
+    def read_csv_to_df(self, key, decoding = 'utf-8', sep = ','):
+        self._logger.info('Reading file %s/%s/%s', self.endpoint_url,self._bucket.name, key)
+        csv_obj = self._bucket.Object(key=key).get().get('Body').read().decode(decoding)
         data = StringIO(csv_obj)
         df = pd.read_csv(data, delimiter=sep)
         return df
 
-    def write_df_to_s3(bucket, df, key):
-        out_buffer = BytesIO()
-        df.to_parquet(out_buffer, index=False)
-        bucket.put_object(Body=out_buffer.getvalue(), Key=key)
+    def write_df_to_s3(self, data_frame: pd.DataFrame, key: str, file_format: str):
+        """
+        writing a Pandas DataFrame to S3
+        supported formats: .csv, .parquet
+
+        :data_frame: Pandas DataFrame that should be written
+        :key: target key of the saved file
+        :file_format: format of the saved file
+        """
+        if data_frame.empty:
+            self._logger.info('The dataframe is empty! No file will be written!')
+            return None
+        if file_format == S3FileTypes.CSV.value:
+            out_buffer = StringIO()
+            data_frame.to_csv(out_buffer, index=False)
+            return self.__put_object(out_buffer, key)
+        if file_format == S3FileTypes.PARQUET.value:
+            out_buffer = BytesIO()
+            data_frame.to_parquet(out_buffer, index=False)
+            return self.__put_object(out_buffer, key)
+        self._logger.info('The file format %s is not '
+                          'supported to be written to s3!', file_format)
+        raise WrongFormatException
+
+    def __put_object(self, out_buffer: StringIO or BytesIO, key: str):
+        """
+        Helper function for self.write_df_to_s3()
+
+        :out_buffer: StringIO | BytesIO that should be written
+        :key: target key of the saved file
+        """
+        self._logger.info('Writing file to %s/%s/%s', self.endpoint_url, self._bucket.name, key)
+        self._bucket.put_object(Body=out_buffer.getvalue(), Key=key)
         return True
 
     def list_files_in_prefix(self, prefix: str):
